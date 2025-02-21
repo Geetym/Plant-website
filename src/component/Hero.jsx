cf029748fb72a6dcb371b9d2eed2b83d3459f08c
God's time Maklu <godstimemaklu3@gmail.com>
2025-02-21 13:01:22 +0100

    update

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
     onSwiper={(swiper) => console.log(swiper)}
     onSlideChange={() => console.log('slide change')}
   >
-    <SwiperSlide>Slide 1</SwiperSlide>
+    <SwiperSlide><img src="" alt="" /></SwiperSlide>
     <SwiperSlide>Slide 2</SwiperSlide>
     <SwiperSlide>Slide 3</SwiperSlide>
     <SwiperSlide>Slide 4</SwiperSlide>
