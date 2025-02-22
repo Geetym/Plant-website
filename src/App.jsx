d341fff944b99276691c9d1160715255de638ec6
God's time Maklu <godstimemaklu3@gmail.com>
2025-02-23 00:05:28 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
           </>
         } 
       />
-           <Route path='/blog' element={<Blog />} />
+          <Route path='/blog' element={<Blog />} />
 
     </Routes>
   </>
