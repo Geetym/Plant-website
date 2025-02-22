993bf904031e4d7f0b4165f3dc992521abf97144
God's time Maklu <godstimemaklu3@gmail.com>
2025-02-22 23:25:32 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,9 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
- ÷
+    <BrowserRouter>
+       <App />
+    </BrowserRouter>
+   
   </StrictMode>,
 )
