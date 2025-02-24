b8a512d2afabdf61bafc1b872d2ebd0265a9120d
God's time Maklu <godstimemaklu3@gmail.com>
2025-02-24 21:42:37 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
         } 
       />
       <Route path='/blog' element={<Blog />} />
-      <Route path='/about' element={<Abo}
+      <Route path='/about' element={<Abou}
     </Routes>
   </>
   )
